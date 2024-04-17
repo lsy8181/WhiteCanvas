22376e28e1afa5ee1f6ac657493f3107881cff0e
hayanLee <hayanlee.dev@gmail.com>
2024-04-17 14:48:26 +0900

    personal 연결오류

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,11 +1,9 @@
 // Firebase SDK 라이브러리 가져오기
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
 import {
+    getFirestore,
     collection,
     addDoc,
-} from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
-import {
     getDocs,
     doc,
     deleteDoc,
@@ -26,46 +24,67 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-$('.submit').click(async function () {
-    let inputName = $('.yourname').val();
-    let inputContent = $('.yourcontent').val();
-    let boxName = $('.boxname').val();
-    let boxContent = $('.boxcontent').val();
+console.log(window.location.pathname);
 
-    let doc = {
-        name: inputName,
-        content: inputContent,
-    };
+if (window.location.pathname === '/index.html') {
+    getGuestBooks();
+} else if (window.location.pathname === '/personalPage_main.html') {
+    console.log('dd');
+    getMemberData();
+}
 
-    await addDoc(collection(db, 'review'), doc);
-    alert('등록되었습니다.');
-    window.location.reload();
-});
-
-let docs = await getDocs(collection(db, 'review'));
-docs.forEach((document) => {
-    let row = document.data();
-    let thisname = row['name'];
-    let thiscontent = row['content'];
-
-    let temp_html = `
-          <div id=${document.id} class="box">
-              <button class="delete">x</button>
-              <div class="boxinner">
-                  <div class="boxname">${thisname}</div>
-                  <div class="boxcontent">${thiscontent}</div>
-              </div>
-          </div>`;
-
-    $('.resultbox').append(temp_html);
-
-    $(`#${document.id} .delete`).click(async function (e) {
-        if (confirm('삭제하시겠습니까?')) {
-            e.target.parentElement.remove();
-            if (e.target.parentElement.id === document.id) {
-                await deleteDoc(doc(db, 'review', document.id));
-            }
-        } else {
-        }
+async function getGuestBooks() {
+    console.log('게스트북');
+    $('.submit').click(async function () {
+        let inputName = $('.yourname').val();
+        let inputContent = $('.yourcontent').val();
+        let boxName = $('.boxname').val();
+        let boxContent = $('.boxcontent').val();
+        let doc = {
+            name: inputName,
+            content: inputContent,
+        };
+        await addDoc(collection(db, 'review'), doc);
+        alert('등록되었습니다.');
+        window.location.reload();
     });
+    let docs = await getDocs(collection(db, 'review'));
+    docs.forEach((document) => {
+        let row = document.data();
+        let thisname = row['name'];
+        let thiscontent = row['content'];
+        let temp_html = `
+              <div id=${document.id} class="box">
+                  <button class="delete">x</button>
+                  <div class="boxinner">
+                      <div class="boxname">${thisname}</div>
+                      <div class="boxcontent">${thiscontent}</div>
+                  </div>
+              </div>`;
+        $('.resultbox').append(temp_html);
+        $(`#${document.id} .delete`).click(async function (e) {
+            if (confirm('삭제하시겠습니까?')) {
+                e.target.parentElement.remove();
+                if (e.target.parentElement.id === document.id) {
+                    await deleteDoc(doc(db, 'review', document.id));
+                }
+            } else {
+            }
+        });
+    });
+}
+
+// 멤버 정보 가져오기
+async function getMemberData() {
+    console.log('멤버');
+    const memberDocRef = await getDocs(collection(db, 'members'));
+    memberDocRef.forEach((doc) => {
+        console.log(doc.data());
+    });
+}
+
+console.log('멤버');
+const memberDocRef = await getDocs(collection(db, 'members'));
+memberDocRef.forEach((doc) => {
+    console.log(doc.data());
 });
