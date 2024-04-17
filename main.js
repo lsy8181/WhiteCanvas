11e66d30075cf34f8995b347cdbcc460039dd523
Jiyoung <jiyoung990602@gmail.com>
2024-04-17 14:19:42 +0900

    방명록

---
+++ main.js
@@ -0,0 +1,15 @@
+// const inputName = document.querySelector('.yourname');
+// const inputContent = document.querySelector('.yourcontent');
+// const button = document.querySelector('button');
+// const boxName = document.querySelector('.boxname');
+// const boxContent = document.querySelector('.boxcontent');
+
+// button.addEventListener('click',function(){
+//     boxName.textContent = inputName.value;
+//     boxContent.textContent = inputContent.value;
+
+//     inputName.value = '';
+//     inputContent.value = '';
+// })
+
+// function createBox(){}
